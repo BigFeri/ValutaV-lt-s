505ed2a61e8eae92a315fee6ddd9b79cb3c401e5
BigFeri <ferenc0516@gmail.com>
2024-04-28 13:40:06 +0200

    index.html kész

--- my.js
+++ my.js
@@ -1,45 +1,42 @@
-const {createApp} = Vue;
+const { createApp } = Vue
 
 createApp({
-    data() {
-        return {
-        ValutaAtvaltasa: [],
-        valutak: [
-            { name : "USA dollar", szimbolum: "$", arujegy: "USD"},
-            { name : "Magyar Forint", szimbolum: "Ft", arujegy: "HUf"},
-            { name : "Euro", szimbolum: "€", arujegy: "EUR"},
-            { name : "Orosz Rubel", szimbolum: "₽", arujegy: "RUB"},
-        ],
-        apikey: "fca_live_NaQq6PqJfc2yuvykGW4JBKRm8npS8FCdSlKkqfix",
-        value: 1,
-        what: "HUF",
-        to: "HUF",
-        total: null
-    } ; 
+  data() {
+    return {
+      ValutaAtvaltasok: [],
+      valutak: [
+        { name: "USA dollar", szimbolum: "$", arujegy: "USD" },
+        { name: "Magyar Forint", szimbolum: "Ft", arujegy: "HUf" },
+        { name: "Euro", szimbolum: "€", arujegy: "EUR" },
+        { name: "Orosz Rubel", szimbolum: "₽", arujegy: "RUB" },
+      ],
+      apikey: "fca_live_NaQq6PqJfc2yuvykGW4JBKRm8npS8FCdSlKkqfix",
+      value: 1,
+      what: "HUF",
+      to: "HUF",
+      total: null,
+    };
+  },
+  async mounted() {
+    await this.onClickCounts();
+  },
+  methods: {
+    async onClickCounts() {
+      await this.getValutaAtvaltasa();
+      this.total = this.value * this.ValutaAtvaltasok[this.to];
     },
-    async mounted() {
-        await this.getValutaAtvaltasa(),
-        await this.onClickCounts();
+    async getValutaAtvaltasa() {
+      const response = await fetch(this.url);
+      const data = await response.json();
+      this.ValutaAtvaltasok = data.data;
     },
-    methods: {
-        async getValutaAtvaltasa(){
-            const response = await fetch(this.url);
-            const data = await response.json();
-            this.ValutaAtvaltasa = data.data;
-        },
-        async onClickCounts(){
-            await this.getValutaAtvaltasa();
-            this.total = this.value * this.ValutaAtvaltasa[this.to];
-        },
+  },
+  computed: {
+    url() {
+      return `https://api.freecurrencyapi.com/v1/latest?apikey=${this.apikey}&currencies=${this.ValutaJegy}&base_currency=${this.what}`;
     },
-    computed: {
-        url(){
-            return `https://api.freecurrencyapi.com/v1/latest?apikey=${this.apikey}&currencies=${this.ValutaJegy}&base_currency=${this.what}`;
-        },
-        ValutaJegy(){
-            return this.valutak
-            .map((c) => c.arujegy)
-            .join("%2C")
-        }
+    ValutaJegy() {
+      return this.valutak.map((c) => c.arujegy).join("%2C");
     },
+  },
 }).mount("#app");
