5117b7adac69cf5bd75be69f8599f4d1fa023953
BigFeri <ferenc0516@gmail.com>
2024-04-28 12:48:52 +0200

    js kész

--- my.js
+++ my.js
@@ -2,6 +2,44 @@ const {createApp} = Vue;
 
 createApp({
     data() {
-        message: "Hello Vue"
-    }
-})
+        return {
+        ValutaAtvaltasa: [],
+        valutak: [
+            { name : "USA dollar", szimbolum: "$", arujegy: "USD"},
+            { name : "Magyar Forint", szimbolum: "Ft", arujegy: "HUf"},
+            { name : "Euro", szimbolum: "€", arujegy: "EUR"},
+            { name : "Orosz Rubel", szimbolum: "₽", arujegy: "RUB"},
+        ],
+        apikey: "fca_live_NaQq6PqJfc2yuvykGW4JBKRm8npS8FCdSlKkqfix",
+        value: 1,
+        what: "HUF",
+        to: "HUF",
+        total: null
+    } ; 
+    },
+    async mounted() {
+        await this.getValutaAtvaltasa(),
+        await this.onClickCounts();
+    },
+    methods: {
+        async getValutaAtvaltasa(){
+            const response = await fetch(this.url);
+            const data = await response.json();
+            this.ValutaAtvaltasa = data.data;
+        },
+        async onClickCounts(){
+            await this.getValutaAtvaltasa();
+            this.total = this.value * this.ValutaAtvaltasa[this.to];
+        },
+    },
+    computed: {
+        url(){
+            return `https://api.freecurrencyapi.com/v1/latest?apikey=${this.apikey}&currencies=${this.ValutaJegy}&base_currency=${this.what}`;
+        },
+        ValutaJegy(){
+            return this.valutak
+            .map((c) => c.arujegy)
+            .join("%2C")
+        }
+    },
+}).mount("#app");
